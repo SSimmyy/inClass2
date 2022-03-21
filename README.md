a01787e67af37a377829149866b9667f873a42cc
SSimmyy <samuel.bang@ndsu.edu>
2022-03-21 11:51:18 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-utkquj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-utkquj)
---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  name = 'Angular ' + VERSION.major;
+
+  showOrHide: boolean = true;
+
+  count: number = 0;
+
+  log: number[] = [];
+
+  onClick() {
+    this.showOrHide = !this.showOrHide;
+    this.log.push(this.count++);
+  }
+}
